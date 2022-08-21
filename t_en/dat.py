54b1021e8ca7fe5cf9f058d8c688e39b4412f3fa
Lacides Miranda <laromicas@hotmail.com>
2022-08-20 21:41:02 -0500

    Added Dat Object, and initial module

---
+++ t_en/dat.py
@@ -0,0 +1,40 @@
+"""
+    TranslatedEnglish Dat class to parse different types of dat files.
+"""
+import re
+
+from repositories.dat import XMLDatFile
+from commands import config
+
+
+class TranslatedEnglishDat(XMLDatFile):
+    """ Translated English Dat class. """
+
+    def initial_parse(self):
+        # pylint: disable=R0801
+        """ Parse the dat file. """
+        name = self.name
+
+        name = name.split('[T-En]')[0].strip()
+        name_array = name.split(' - ')
+
+        company, system = name_array
+        self.company = company
+        self.system = system
+        self.suffix = 'Translated-English'
+        self.overrides()
+
+        if self.modifier or self.system_type:
+            self.preffix = config.get('PREFFIXES', self.modifier or self.system_type, fallback='')
+        else:
+            self.preffix = None
+
+        return [self.preffix, self.company, self.system, self.suffix, self.get_date()]
+
+
+    def get_date(self):
+        """ Get the date from the dat file. """
+        if self.file:
+            result = re.findall(r'\(.*?\)', self.file)
+            self.date = result[len(result)-1][1:-1]
+        return self.date
